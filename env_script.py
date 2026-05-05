689c441033880de1201a0fe0768a93e0c8b20904
M-Abubakr04 <computerex103@gmail.com>
2026-05-05 15:21:10 +0500

    Deploying Tech Nebula Backend to ECS

--- env_script.py
+++ env_script.py
@@ -5,7 +5,7 @@ import requests
 import base64
 from nacl import encoding, public
 
-# ---------------- CONFIG ----------------
+# Here we are configuring our environment
 TOKEN = os.getenv("GITHUB_TOKEN")
 
 if not TOKEN:
@@ -17,13 +17,13 @@ REPO = "tech_nebula_github_actions"
 
 ENV_NAME = input("Enter Environment (production/staging): ").strip()
 
-# ---------------- HEADERS ----------------
+# Header Section
 headers = {
     "Authorization": f"Bearer {TOKEN}",
     "Accept": "application/vnd.github+json"
 }
 
-# ---------------- GET PUBLIC KEY ----------------
+# Looking for Public Key
 url = f"https://api.github.com/repos/{OWNER}/{REPO}/environments/{ENV_NAME}/secrets/public-key"
 
 res = requests.get(url, headers=headers)
@@ -39,14 +39,14 @@ key_id = data["key_id"]
 
 print("Public key fetched successfully")
 
-# ---------------- ENCRYPT FUNCTION ----------------
+# Applying Encryption
 def encrypt_secret(public_key, secret_value):
     public_key = public.PublicKey(public_key.encode(), encoding.Base64Encoder())
     sealed_box = public.SealedBox(public_key)
     encrypted = sealed_box.encrypt(secret_value.encode())
     return base64.b64encode(encrypted).decode()
 
-# ---------------- READ .env ----------------
+# Reading Varibales form .env file present in our local machine or ec2
 env_vars = {}
 
 with open(".env") as f:
@@ -57,7 +57,7 @@ with open(".env") as f:
 
 print("Loaded variables:", list(env_vars.keys()))
 
-# ---------------- PUSH SECRETS ----------------
+# We are Pushing Secrets Now
 for key, value in env_vars.items():
 
     encrypted_value = encrypt_secret(public_key, value)
